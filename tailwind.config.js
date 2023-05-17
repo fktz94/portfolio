61a5a0dd0f084f6f13080d8d2e2d40d3f63af3bd
fktz94 <106633973+fktz94@users.noreply.github.com>
2023-05-17 01:23:16 -0300

    last 'src' update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['index.html', 'docs/*', 'docs/components/*'],
+  content: ['index.html', 'src/*', 'src/components/*'],
   theme: {
     extend: {},
   },
